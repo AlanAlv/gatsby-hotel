a7bcf17525152d3c236eb5ac3af18c567dd87fb1
AlanAlv <alan.alv96@gmail.com>
2020-07-11 02:20:53 -0500

    Nav CSS

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,53 @@
 import React from 'react';
-import { Link }  from 'gatsby'
+import { Link }  from 'gatsby';
+import styled from '@emotion/styled';
 
-const Nav = () => {
+const Nav = styled.nav`
+    display: flex;
+    justify-content: center;
+    padding-bottom: 3rem;
+
+    @media(min-width: 768px) {
+        padding-bottom: 0;
+    }
+`;
+
+const NavLink = styled(Link)`
+    color: #FFFFFF;
+    font-size: 1.6rem;
+    font-weight: 700;
+    line-height: 1rem;
+    font-family: 'PT Sans', sans-serif;
+    text-decoration: none;
+    padding: 1rem;
+    padding-right: 1rem;
+
+    &:last-of-type {
+        margin-right: 0;
+    }
+    &.current-page {
+        border-bottom: 2px solid #FFFFFF;
+    }
+`;
+
+const Navigation = () => {
     return ( 
-        <nav>
-            <Link to={'/'}>Home</Link>
-            <Link to={'/about'}>About us</Link>
-        </nav>
+        <Nav>
+            <NavLink 
+                to={'/'}
+                activeClassName="current-page"
+            >
+                Home
+            </NavLink>
+
+            <NavLink 
+                to={'/about'}
+                activeClassName="current-page"
+            >
+                About us
+            </NavLink>
+        </Nav>
      );
 }
  
-export default Nav;
+export default Navigation;
