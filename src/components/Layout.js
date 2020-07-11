76a8734b7a0bd7421bdf70c7271df76bfa48f4fa
AlanAlv <alan.alv96@gmail.com>
2020-07-11 01:41:10 -0500

    Global CSS

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import Header from '../components/Header';
-import Helmet from 'react-helmet'
+import Helmet from 'react-helmet';
+import { Global, css } from '@emotion/core';
 
 const Layout = (props) => {
     return (
         <>
+            <Global 
+                styles={css`
+
+                    html {
+                        font-size: 62.5%;
+                    }
+                    body {
+                        font-size: 16px;
+                        font-size: 1.6rem;
+                        line-height: 1.5;
+                    }
+                    h1, h2, h3 {
+                        margin: 0;
+                        line-height: 1.5;
+                    }
+                    h1, h2 {
+                        font-family: 'Roboto', serif;
+                    }
+                    h3 {
+                        font-family: 'PT Sans', sans-serif;
+                    }
+                    ul {
+                        list-style: none;
+                        margin: 0;
+                        padding: 0;
+                    }
+                
+                `}
+            />
             <Helmet>
                 <title>Gatsby Hotel</title>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/
