a9a23907ced9a288a14b46295592bf58d056ede9
AlanAlv <alan.alv96@gmail.com>
2020-07-11 01:31:14 -0500

    Added Helmet

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,19 @@
 import React from 'react';
-import Header from '../components/Header'
+import Header from '../components/Header';
+import Helmet from 'react-helmet'
 
 const Layout = (props) => {
     return (
         <>
+            <Helmet>
+                <title>Gatsby Hotel</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/
+                    ajax/libs/normalize/8.0.1/normalize.min.css"/>
+                <link href="https://fonts.googleapis.com/css2?family=
+                    PT+Sans:wght@400;700&family=Roboto:wght@400;700&
+                    display=swap" rel="stylesheet"/>
+            </Helmet>
+
             <Header />
             {props.children}
         </>
