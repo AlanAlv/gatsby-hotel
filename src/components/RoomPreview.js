4afc8500b1fd2548cd916aa16a46b26cc18041c9
AlanAlv <alan.alv96@gmail.com>
2020-07-14 02:17:38 -0500

    Room Preview CSS

--- src/components/RoomPreview.js
+++ src/components/RoomPreview.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import Image from 'gatsby-image';
+import { css } from '@emotion/core'
+import styled from '@emotion/styled'
+import { Link } from 'gatsby'
+
+const Button = styled(Link)`
+    margin-top: 2rem;
+    padding: 1rem;
+    background-color: rgba(44, 62, 80, .85);
+    width: 100%;
+    color: #FFF;
+    display: block;
+    text-decoration: none;
+    text-transform: uppercase;
+    font-weight: 700;
+    text-align: center;
+`;
+
+
 
 const RoomPreview = ({room}) => {
     
     const { content, image, title, slug } = room;
     return ( 
-        <div>
+        <div
+            css={css`
+                border: 1px solid #E1E1E1;
+                margin-bottom: 2rem;
+            `}
+        >
             <Image
                 fluid={image.fluid}
             />
-            <div>
-                <h3>{title}</h3>
+            <div
+                css={css`
+                    padding: 3rem;
+                `}
+            >
+                <h3
+                    css={css`
+                        font-size: 3rem;
+                    `}
+                
+                >{title}</h3>
                 <p>{content}</p>
+
+                <Button to={slug}>See Room</Button>
             </div>
         </div>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { css } from '@emotion/core';
+import styled from '@emotion/styled';
 import Layout from '../components/layout';
 import HotelImage from '../components/HotelImage';
 import HomeContent from '../components/HomeContent';
@@ -8,6 +9,18 @@ import useRooms from '../hooks/useRooms';
 
 
 const IndexPage = () => { 
+
+  const RoomsList = styled.ul`
+    max-width: 1200px;
+    widows: 95%;
+    margin: 4rem auto 0 auto;
+
+    @media(min-width: 768px){
+      display: grid;
+      grid-template-columns: repeat(3, 1fr); 
+      column-gap: 3rem;
+    }
+  `;
 
   const rooms = useRooms();
 
@@ -27,14 +40,14 @@ const IndexPage = () => {
         Our rooms
       </h2>
 
-      <ul>
+      <RoomsList>
         {rooms.map(room => (
           <RoomPreview 
             key={room.id}
             room={room}
           />
         ))}
-      </ul>
+      </RoomsList>
     </Layout>
   )
 }
