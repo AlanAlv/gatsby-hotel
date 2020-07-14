ba93e379a226f443a012999e201935f560f0f7ba
AlanAlv <alan.alv96@gmail.com>
2020-07-14 01:55:25 -0500

    Room Preview Component

---
+++ src/components/RoomPreview.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const RoomPreview = () => {
+    return ( 
+        <h1>Room</h1>
+     );
+}
+ 
+export default RoomPreview;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,35 @@
-import React from "react"
+import React from "react";
+import { css } from '@emotion/core';
 import Layout from '../components/layout';
 import HotelImage from '../components/HotelImage';
 import HomeContent from '../components/HomeContent';
-import useRooms from '../hooks/useRooms'
+import RoomPreview from '../components/RoomPreview';
+import useRooms from '../hooks/useRooms';
 
 
 const IndexPage = () => { 
 
-  useRooms();
+  const rooms = useRooms();
+
+  console.log(rooms);
   return (
     <Layout>
       <HotelImage />
       <HomeContent />
+
+      <h2
+        css={css`
+          text-align: center;
+          margin-top: 5rem;
+          font-size: 3rem;
+        `}
+      >
+        Our rooms
+      </h2>
+
+        {rooms.map(room => (
+          <RoomPreview />
+        ))}
       
     </Layout>
   )
